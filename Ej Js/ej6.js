3ef8c137024c23c3e1ac31989e93353a4e636a66
maximilianocup <63514082+maximilianocup@users.noreply.github.com>
2020-08-30 15:39:56 -0300

    Delete ej6.js

--- Ej Js/ej6.js
+++
@@ -1,7 +0,0 @@
-var quienesAprobaron = [[8,6,2,4],[7,9,4,5],[6,2,4,2],[9,6,7,10]];
-
-var resultado = quienesAprobaron.filter(i => i>=4);
-
-
-console.log(resultado);
-//console.log(quienesAprobaron.filter(i => i>=4));
