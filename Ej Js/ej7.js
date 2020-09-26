bf56b7950c8a515266d3b821b22d1fba8cba2b53
maximilianocup <63514082+maximilianocup@users.noreply.github.com>
2020-09-25 23:00:04 -0300

    Update ej7.js

--- Ej Js/ej7.js
+++ Ej Js/ej7.js
@@ -1,7 +1,7 @@
 var hayAlgunNegativo = [2,-3,9];
 
 function negativo(array){
-    console.log(hayAlgunNegativo.some(i => i <=0));
+    console.log(hayAlgunNegativo.some(i => i <0));
 }
 
 negativo(hayAlgunNegativo);
